55778fef9baa4f56d0955f6f3a679019cad04403
WheatleyZ <zhengsl7749@live.com>
2018-04-10 17:56:27 +0800

    excluded node_modules

--- DMS.Node/app.js
+++ DMS.Node/app.js
@@ -6,7 +6,8 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var managing = require('./routes/managing');
+var others = require('./routes/requests');
 
 var app = express();
 
@@ -15,7 +16,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hjs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public/images', 'brand.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -23,7 +24,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/managing', managing);
+app.use('/requests', others);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- DMS.Node/routes/index.js
+++ DMS.Node/routes/index.js
@@ -1,9 +1,54 @@
 var express = require('express');
 var router = express.Router();
+var sql = require('mysql');
+var connection = sql.createConnection({
+  host: 'localhost',
+  user: 'dms',
+  password: '1234',
+  database: 'dormitory'
+});
+connection.connect();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  connection.query('select * from dormitory.students', function (error, results, fields) {
+    
+  });
+
+  res.render('index', {
+    title: "宿舍管理系统",
+    managing: 0,
+    room: [{
+      roomname: "Lorem Ipsum",
+      dormno: "6",
+      roomno: "606",
+      roomcount: "4",
+      repairrequest: 2,
+      repairitem: [
+        "日光灯", "下水道"
+      ],
+      student: [{
+        num: 1,
+        name: "Mark",
+        id: 0001,
+        gender: "男"
+      }, {
+        num: 2,
+        name: "Jacob",
+        id: 0002,
+        gender: "男"
+      }, {
+        num: 3,
+        name: "Larry",
+        id: 0003,
+        gender: "女"
+      }, {
+        num: 4,
+        name: "Mary",
+        id: 0004,
+        gender: "女"
+      }]
+    }, ]
+  });
 });
 
 module.exports = router;
