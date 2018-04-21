63ced4178caa4259cd823f59d7580e74ea1e610b
WheatleyZ <zhengsl7749@live.com>
2018-04-22 03:15:28 +0800

    update .gitignore

---
+++ DMS.Node/routes/managing.js
@@ -0,0 +1,79 @@
+var express = require('express');
+var router = express.Router();
+var sql = require('mysql');
+var connection = sql.createConnection({
+	host: 'localhost',
+	user: 'dms',
+	password: '1234',
+	database: 'dormitory'
+});
+connection.connect();
+
+router.get('/', function (req, res, next) {
+	res.render('managing', {
+		title: "宿舍管理系统",
+		managing: 1,
+		room: [{
+			roomname: "Lorem Ipsum",
+			dormno: "6",
+			roomno: "606",
+			roomcount: "4",
+			repairrequest: 2,
+			repairitem: [
+				"日光灯", "下水道"
+			],
+			student: [{
+				num: 1,
+				name: "Mark",
+				id: 0001,
+				gender: "男"
+			}, {
+				num: 2,
+				name: "Jacob",
+				id: 0002,
+				gender: "男"
+			}, {
+				num: 3,
+				name: "Larry",
+				id: 0003,
+				gender: "女"
+			}, {
+				num: 4,
+				name: "Mary",
+				id: 0004,
+				gender: "女"
+			}]
+		}]
+	});
+});
+
+router.get('/repair', function (req, res, next) {
+	res.render('managing', {
+		title: "宿舍管理系统-维修申请",
+		repair: 1,
+		repairdetail: [{
+			id: 1,
+			name: "李四",
+			room: "606",
+			item: "日光灯",
+			reason: "灯管烧坏了"
+		}]
+	});
+});
+router.get('/visit', function (req, res, next) {
+	res.render('managing', {
+		title: "宿舍管理系统-访客申请",
+		visitor: 1,
+		visitordetail: [{
+			id: 1,
+			name: "张三",
+			arrive: "2018/01/01 12:00",
+			leave: "2018/01/01 13:00",
+			contact: "13012341234",
+			reason: "看望学生"
+		}]
+	});
+});
+
+
+module.exports = router;
---
+++ DMS.Node/routes/requests.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var router = express.Router();
+var sql = require('mysql');
+var connection = sql.createConnection({
+	host: 'localhost',
+	user: 'dms',
+	password: '1234',
+	database: 'dormitory'
+});
+connection.connect();
+
+router.get('/', function (req, res, next) {
+	res.render('requests', {
+		title: "宿舍管理系统-申请选择",
+		home: 1
+	});
+});
+router.get('/repair', function (req, res, next) {
+	res.render('requests', {
+		title: "宿舍管理系统-维修申请",
+		repair: 1,
+		repairdetail: [{
+			id: 1,
+			name: "李四",
+			room: "606",
+			item: "日光灯",
+			reason:"灯管烧坏了"
+		}]
+	});
+});
+router.get('/visit', function (req, res, next) {
+	res.render('requests', {
+		title: "宿舍管理系统-访客申请",
+		visitor: 1,
+		visitordetail: [{
+			id: 1,
+			name: "张三",
+			arrive: "2018/01/01 12:00",
+			leave: "2018/01/01 13:00",
+			contact: "13012341234",
+			reason:"看望学生"
+		}]
+	});
+});
+
+module.exports = router;
