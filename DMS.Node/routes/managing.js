278082b7665f94bcd8f47850e74cda61a5371248
WheatleyZ <zhengsl7749@live.com>
2018-06-12 09:26:40 +0800

    new project added

--- DMS.Node/routes/managing.js
+++ DMS.Node/routes/managing.js
@@ -13,7 +13,8 @@ router.get('/', function (req, res, next) {
 	res.render('managing', {
 		title: "宿舍管理系统",
 		managing: 1,
-		room: [{
+		room: [ {
+			id: 1,
 			roomname: "Lorem Ipsum",
 			dormno: "6",
 			roomno: "606",
@@ -22,7 +23,7 @@ router.get('/', function (req, res, next) {
 			repairitem: [
 				"日光灯", "下水道"
 			],
-			student: [{
+			student: [ {
 				num: 1,
 				name: "Mark",
 				id: 0001,
@@ -42,8 +43,349 @@ router.get('/', function (req, res, next) {
 				name: "Mary",
 				id: 0004,
 				gender: "女"
-			}]
-		}]
+			} ]
+		}, {
+			id: 2,
+			roomname: "Lorem Ipsum",
+			dormno: "6",
+			roomno: "606",
+			roomcount: "4",
+			repairrequest: 2,
+			repairitem: [
+				"日光灯", "下水道"
+			],
+			student: [ {
+				num: 1,
+				name: "Mark",
+				id: 0001,
+				gender: "男"
+			}, {
+				num: 2,
+				name: "Jacob",
+				id: 0002,
+				gender: "男"
+			}, {
+				num: 3,
+				name: "Larry",
+				id: 0003,
+				gender: "女"
+			}, {
+				num: 4,
+				name: "Mary",
+				id: 0004,
+				gender: "女"
+			} ]
+		}, {
+			id: 3,
+			roomname: "Lorem Ipsum",
+			dormno: "6",
+			roomno: "606",
+			roomcount: "4",
+			repairrequest: 2,
+			repairitem: [
+				"日光灯", "下水道"
+			],
+			student: [ {
+				num: 1,
+				name: "Mark",
+				id: 0001,
+				gender: "男"
+			}, {
+				num: 2,
+				name: "Jacob",
+				id: 0002,
+				gender: "男"
+			}, {
+				num: 3,
+				name: "Larry",
+				id: 0003,
+				gender: "女"
+			}, {
+				num: 4,
+				name: "Mary",
+				id: 0004,
+				gender: "女"
+			} ]
+		}, {
+			id: 4,
+			roomname: "Lorem Ipsum",
+			dormno: "6",
+			roomno: "606",
+			roomcount: "4",
+			repairrequest: 2,
+			repairitem: [
+				"日光灯", "下水道"
+			],
+			student: [ {
+				num: 1,
+				name: "Mark",
+				id: 0001,
+				gender: "男"
+			}, {
+				num: 2,
+				name: "Jacob",
+				id: 0002,
+				gender: "男"
+			}, {
+				num: 3,
+				name: "Larry",
+				id: 0003,
+				gender: "女"
+			}, {
+				num: 4,
+				name: "Mary",
+				id: 0004,
+				gender: "女"
+			} ]
+		}, {
+			id: 5,
+			roomname: "Lorem Ipsum",
+			dormno: "6",
+			roomno: "606",
+			roomcount: "4",
+			repairrequest: 2,
+			repairitem: [
+				"日光灯", "下水道"
+			],
+			student: [ {
+				num: 1,
+				name: "Mark",
+				id: 0001,
+				gender: "男"
+			}, {
+				num: 2,
+				name: "Jacob",
+				id: 0002,
+				gender: "男"
+			}, {
+				num: 3,
+				name: "Larry",
+				id: 0003,
+				gender: "女"
+			}, {
+				num: 4,
+				name: "Mary",
+				id: 0004,
+				gender: "女"
+			} ]
+		}, {
+			id: 6,
+			roomname: "Lorem Ipsum",
+			dormno: "6",
+			roomno: "606",
+			roomcount: "4",
+			repairrequest: 2,
+			repairitem: [
+				"日光灯", "下水道"
+			],
+			student: [ {
+				num: 1,
+				name: "Mark",
+				id: 0001,
+				gender: "男"
+			}, {
+				num: 2,
+				name: "Jacob",
+				id: 0002,
+				gender: "男"
+			}, {
+				num: 3,
+				name: "Larry",
+				id: 0003,
+				gender: "女"
+			}, {
+				num: 4,
+				name: "Mary",
+				id: 0004,
+				gender: "女"
+			} ]
+		}, {
+			id: 7,
+			roomname: "Lorem Ipsum",
+			dormno: "6",
+			roomno: "606",
+			roomcount: "4",
+			repairrequest: 2,
+			repairitem: [
+				"日光灯", "下水道"
+			],
+			student: [ {
+				num: 1,
+				name: "Mark",
+				id: 0001,
+				gender: "男"
+			}, {
+				num: 2,
+				name: "Jacob",
+				id: 0002,
+				gender: "男"
+			}, {
+				num: 3,
+				name: "Larry",
+				id: 0003,
+				gender: "女"
+			}, {
+				num: 4,
+				name: "Mary",
+				id: 0004,
+				gender: "女"
+			} ]
+		}, {
+			id: 8,
+			roomname: "Lorem Ipsum",
+			dormno: "6",
+			roomno: "606",
+			roomcount: "4",
+			repairrequest: 2,
+			repairitem: [
+				"日光灯", "下水道"
+			],
+			student: [ {
+				num: 1,
+				name: "Mark",
+				id: 0001,
+				gender: "男"
+			}, {
+				num: 2,
+				name: "Jacob",
+				id: 0002,
+				gender: "男"
+			}, {
+				num: 3,
+				name: "Larry",
+				id: 0003,
+				gender: "女"
+			}, {
+				num: 4,
+				name: "Mary",
+				id: 0004,
+				gender: "女"
+			} ]
+		}, {
+			id: 9,
+			roomname: "Lorem Ipsum",
+			dormno: "6",
+			roomno: "606",
+			roomcount: "4",
+			repairrequest: 2,
+			repairitem: [
+				"日光灯", "下水道"
+			],
+			student: [ {
+				num: 1,
+				name: "Mark",
+				id: 0001,
+				gender: "男"
+			}, {
+				num: 2,
+				name: "Jacob",
+				id: 0002,
+				gender: "男"
+			}, {
+				num: 3,
+				name: "Larry",
+				id: 0003,
+				gender: "女"
+			}, {
+				num: 4,
+				name: "Mary",
+				id: 0004,
+				gender: "女"
+			} ]
+		}, {
+			id: 10,
+			roomname: "Lorem Ipsum",
+			dormno: "6",
+			roomno: "606",
+			roomcount: "4",
+			repairrequest: 2,
+			repairitem: [
+				"日光灯", "下水道"
+			],
+			student: [ {
+				num: 1,
+				name: "Mark",
+				id: 0001,
+				gender: "男"
+			}, {
+				num: 2,
+				name: "Jacob",
+				id: 0002,
+				gender: "男"
+			}, {
+				num: 3,
+				name: "Larry",
+				id: 0003,
+				gender: "女"
+			}, {
+				num: 4,
+				name: "Mary",
+				id: 0004,
+				gender: "女"
+			} ]
+		}, {
+			id: 11,
+			roomname: "Lorem Ipsum",
+			dormno: "6",
+			roomno: "606",
+			roomcount: "4",
+			repairrequest: 2,
+			repairitem: [
+				"日光灯", "下水道"
+			],
+			student: [ {
+				num: 1,
+				name: "Mark",
+				id: 0001,
+				gender: "男"
+			}, {
+				num: 2,
+				name: "Jacob",
+				id: 0002,
+				gender: "男"
+			}, {
+				num: 3,
+				name: "Larry",
+				id: 0003,
+				gender: "女"
+			}, {
+				num: 4,
+				name: "Mary",
+				id: 0004,
+				gender: "女"
+			} ]
+		}, {
+			id: 12,
+			roomname: "Lorem Ipsum",
+			dormno: "6",
+			roomno: "606",
+			roomcount: "4",
+			repairrequest: 2,
+			repairitem: [
+				"日光灯", "下水道"
+			],
+			student: [ {
+				num: 1,
+				name: "Mark",
+				id: 0001,
+				gender: "男"
+			}, {
+				num: 2,
+				name: "Jacob",
+				id: 0002,
+				gender: "男"
+			}, {
+				num: 3,
+				name: "Larry",
+				id: 0003,
+				gender: "女"
+			}, {
+				num: 4,
+				name: "Mary",
+				id: 0004,
+				gender: "女"
+			} ]
+		}, ]
 	});
 });
 
